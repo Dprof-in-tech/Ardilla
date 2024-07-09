5e55d0c8c48a9efabdabc433f380c899b05b79a7
Isaac Onyemaechi <Amaechiisaac450@gmail.com>
2024-07-09 19:07:14 +0100

    fix to the pwa

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
 import hero from "../assets/hero.svg";
+import { Link } from "react-router-dom";
+
 
 const Hero = () => {
     return (
@@ -11,7 +13,7 @@ const Hero = () => {
                 <h1 className="text-white text-[3.5rem] lg:text-[5rem] font-bold text-center mt-10">Your Access To More</h1>
                 <p className="text-white text-center lg:max-w-[60%]">Ardilla helps you save and invest in achieving your financial goals, we have the most flexible and seamless way to grow your funds.</p>
                 <div className="flex justify-center mt-8">
-                    <button className="bg-[#ffffff] rounded-full px-12 py-3 text-[#240053] hover:bg-[#8807F7] hover:text-white">Get Started</button>
+                    <Link href='/interest'><button className="bg-[#ffffff] rounded-full px-12 py-3 text-[#240053] hover:bg-[#8807F7] hover:text-white">Get Started</button></Link>
                 </div>
 
                 <div className="flex  items-center justify-center h-[460px] lg:w-auto w-[full]">
